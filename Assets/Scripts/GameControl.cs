c12328808ec266e2ea87854cb2919abbd938e213
crassSandwich <christopherjanidlo@gmail.com>
2017-04-22 19:04:30 -0400

    Working level spawn framework

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+	public Dictionary<Vector2, TileController> tiles; //map positions to tiles
+	public Texture2D TileAtlas;
+	public Sprite[] TileSprites;
+
+	private GameObject TilePrefab;
+
+	// Use this for initialization
+	void Start () {
+		tiles = new Dictionary<Vector2, TileController> ();
+		TileSprites = Resources.LoadAll<Sprite> (TileAtlas.name);
+		TilePrefab = (GameObject) Resources.Load ("Tile");
+
+		CreateTile (Vector2.zero, 6);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void CreateTile (Vector2 position, int n) {
+		TileController tile = Instantiate (TilePrefab).GetComponent<TileController>();
+		tile.Initialize (position, 0);
+		tiles.Add (position, tile);
+	}
+
+	public TileController GetTileAtPosition (Vector2 position) {
+		TileController tile;
+		if (tiles.TryGetValue (position, out tile))
+			return tile;
+		else
+			return null;
+	}
+
+	//more user-friendly way to index sprites. if a direction is set to true, we will return a sprite with a path heading in that direction
+	public Sprite GetTileSpriteByDirection(bool north, bool east, bool south, bool west) {
+		if (north && east && south && west)
+			return TileSprites [0];
+		else if (north && east && south && !west)
+			return TileSprites [13];
+		else if (north && east && !south && west)
+			return TileSprites [12];
+		else if (north && !east && south && west)
+			return TileSprites [3];
+		else if (!north && east && south && west)
+			return TileSprites [2];
+		else if (!north && !east && south && west)
+			return TileSprites [4];
+		else if (!north && east && !south && west)
+			return TileSprites [5];
+		else if (!north && east && south && !west)
+			return TileSprites [6];
+		else if (north && !east && !south && west)
+			return TileSprites [7];
+		else if (north && !east && south && !west)
+			return TileSprites [15];
+		else if (north && east && !south && !west)
+			return TileSprites [10];
+		else if (north && !east && !south && !west)
+			return TileSprites [8];
+		else if (!north && east && !south && !west)
+			return TileSprites [9];
+		else if (!north && !east && south && !west)
+			return TileSprites [11];
+		else if (!north && !east && !south && west)
+			return TileSprites [14];
+		else //if (!north && !east && !south && !west)
+			return TileSprites [1];
+	}
+}
---
+++ Assets/Scripts/TileController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour {
+
+	public static GameControl MainControlScript;
+	public Vector2 position;
+	public TileController m_northTile;
+	public TileController m_eastTile;
+	public TileController m_southTile;
+	public TileController m_westTile;
+
+	private SpriteRenderer spriteR;
+
+	public void Initialize (Vector2 position, int n) {
+		MainControlScript = GameObject.Find ("GameController").GetComponent<GameControl> ();
+		this.position = position;
+		transform.position = new Vector3 (position.x, position.y);
+		spriteR = gameObject.GetComponent<SpriteRenderer> ();
+		m_northTile = null;
+     	m_eastTile = null;
+		m_southTile = null;
+		m_westTile = null;
+		CheckSurroundings ();
+		if (n > 0)
+			BranchingGrowth ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckSurroundings ();
+	}
+
+
+	//check if there are any new neighboring tiles, and update this one appropriately 
+	void CheckSurroundings () {
+		bool change = false;
+		if (m_northTile == null) {
+			m_northTile = MainControlScript.GetTileAtPosition (new Vector2 (position.x, position.y + 1));
+			if (m_northTile != null)
+				change = true;
+		}
+		if (m_eastTile == null) {
+			m_eastTile = MainControlScript.GetTileAtPosition (new Vector2 (position.x + 1, position.y));
+			if (m_eastTile != null)
+				change = true;
+		}
+		if (m_southTile == null) {
+			m_southTile = MainControlScript.GetTileAtPosition (new Vector2 (position.x, position.y - 1));
+			if (m_southTile != null)
+				change = true;
+		}
+		if (m_westTile == null) {
+			m_westTile = MainControlScript.GetTileAtPosition (new Vector2 (position.x - 1, position.y));
+			if (m_westTile != null)
+				change = true;
+		}
+		if (change) {
+			spriteR.sprite = MainControlScript.GetTileSpriteByDirection (m_northTile != null, m_eastTile != null, m_southTile != null, m_westTile != null);
+		}
+			
+	}
+
+	void BranchingGrowth (int n) {
+
+	}
+
+}
